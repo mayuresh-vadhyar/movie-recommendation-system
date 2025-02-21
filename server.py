1c9c1682e7770a771623c4dc3c0272a729c7004d
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-02-22 02:21:48 +0530

    Completed API to get movie index by title

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from models.DataFrame import DataFrame
 from models.ContentBasedFiltering import ContentBasedFiltering
 
 app = Flask(__name__)
+df = DataFrame()
 cbf = ContentBasedFiltering()
 
 @app.route('/movies/index', methods=['GET'])
 def getMovieIndexByTitle():
-  return jsonify({'result': 'success'})
+  try:
+    title = request.args.get('title')
+    response = str(df.getIndexFromTitle(title))
+    return jsonify({'result': 'success', 'response': response})
+  except Exception as E:
+    return jsonify({'result': 'failure', 'error': str(E)})
 
 @app.route('/movies/closest-index', methods=['GET'])
 def getMovieIndexByClosestTitle():
